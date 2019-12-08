ca6cd5af3eb5c0b1fb897c5f7e41e4b4bf432a43
Jatin Arora <jaarora45@gmaill.com>
2019-12-08 16:30:06 -0500

    Removed swagger-ui-express package and routes. Added firebase-admin dependency

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express();
 var  path = require("path");
-var swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./package.json');
 var HTTP_PORT = process.env.PORT || 8082;
 
 function onHttpStart() {
@@ -13,8 +11,6 @@ app.get("/", function(req,res){
     res.sendFile(path.join(__dirname,"/src/index.html"));
 });
 
-// route to display api docs using Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // setup another route to listen on /about
 app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname,"/index.html"));
