64320552e34d14ced85b2c54b342c24589a2cb9a
Jatin Arora <jaarora45@gmaill.com>
2020-03-01 10:18:31 -0500

    New File Api.js for storing API related routes

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -5,7 +5,7 @@ const urlBase = "https://api.spoonacular.com/recipes/"
 const apiKey = process.env.secret;
 var rr = require('../routing/routes')
 //const recipeModel = require("../models/UserRecipes");
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes, Op } = require("sequelize");
 /* Important steps to connect to db instance and update it */
 
 var db = new Sequelize('prj666_201a04', 'prj666_201a04', 'faGX@7748', {
@@ -26,9 +26,11 @@ exports.getRecipes = async function (req, res) {
   var response;
   var query = "";
   var dbQuery = {};
+  console.log(req.query);
   for (var key in req.query) {
     if (key == "user") {
       flag = true;
+      continue;
     }
     if (query) {
       query = query + "&" + key + "=" + req.query[key];
@@ -38,41 +40,48 @@ exports.getRecipes = async function (req, res) {
     else
       query = query + key + "=" + req.query[key];
 
-    dbQuery[key] = req.query[key];
+    dbQuery[key] =
+      {
+        [Op.like]: '%' + (req.query[key]) + '%'
+      }
   }
 
   console.log('DB ======= ');
   console.log(dbQuery.key);
   if (!flag) {
-    //  var reqURL = urlBase + 'search?apiKey=' + process.env.secret + '&' + query;
-    const { count, rows } = await UserRecipes.findAndCountAll(
-      {
-        where: req.query
-      }
-
-    );
-    console.log(count);
-    console.log(rows);
+    var reqURL = urlBase + 'search?apiKey=' + process.env.secret + '&' + query;
+    try {
+      response = await axios.get(reqURL);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    res.send(response.data);
   }
   else if (flag) {
-    const { count, rows } = await UserRecipes.findAndCountAll(
-      {
-        where: req.query
-      }
+    try {
+      const { count, rows } = await UserRecipes.findAndCountAll(
+        {
+          where: dbQuery,
+          raw: true
+        }
 
-    );
-    console.log(count);
-    console.log(rows);
+      );
+      rows.forEach(data => {
+        // data.toJSON;
+        var d = JSON.stringify(data);
+        console.log("DATA ::::::::::: ");
+        console.log(d);
+      })
+      response = count + rows;
+      console.log(response);
+    } catch (err) {
+      console.log(err.lineNumber);
+      console.log(err);
+    }
+
+    res.send(response);
   }
-
-  try {
-    // response = await axios.get(reqURL);
-    // console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-  // res.send(response.data);
-
 }
 
 // Display detail page for a specific Author.
