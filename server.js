ea813abc70da8ca7dd7974d6859b5a1253945ab0
student <student@ubuntu18.senecacollege.ca>
2020-02-29 21:37:49 -0500

    Adds Code for VM run

--- server.js
+++ server.js
@@ -11,13 +11,40 @@ const dataService = require('./src/routing/routes');
 
 var cookieParser = require('cookie-parser');
 var HTTP_PORT = process.env.PORT || 8082;
-
-function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
-  process.env.secret = apiSecret.key;
-}
-
+// VM Options
+const vmHostname = '10.102.112.128';
+const vmPort = 10034;
+/*************DO NOT TOUCH ************************************ */
+app.use(express.static(__dirname));
+// setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function (req, res) {
+  console.log('PATH: ', path);
+  res.sendFile(path.join(__dirname, "src/home.html"));
+});
+
+
+
+app.get('/android', function (req, res) {
+  var filePath = '../appBuilds/android/recipeBox.apk'; // Or format the path using the `id` rest param
+  var fileName = "recipeBox.apk"; // The default name the browser will use
+  try {
+    res.download(filePath, fileName);
+  } catch (err) {
+    res.send('File Not Found. Android app build is not available');
+  }
+});
+
+app.get('/ios', function (req, res) {
+  var filePath = '../appBuilds/ios/recipeBox.ipa'; // Or format the path using the `id` rest param
+  var fileName = "recipeBox.ipa"; // The default name the browser will use
+  try {
+    res.download(filePath, fileName);
+  } catch (err) {
+    res.send('File Not Found. iOS app builds are not available yet');
+  }
+});
+/*****************************************************************/
+app.get("/rest-api", function (req, res) {
   res.sendFile(path.join(__dirname, "/src/index.html"));
 });
 
@@ -66,11 +93,15 @@ app.use(function (req, res) {
 
 dataService.initialize()
   .then(() => {
-    app.listen(HTTP_PORT, onHttpStart);
+    app.listen(HTTP_PORT, app.listen);
   }).catch((err) => {
     console.log("Not able to connect to the server");
   });
-
+/*
 onHttpStart = () => {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
+*/
+app.listen(vmPort, vmHostname, () => {
+  console.log('Server is running at http://' + vmHostname + ':' + vmPort + '/');
+});
