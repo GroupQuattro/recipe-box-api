31aa69606d449dc097da9c8ca2cbb5749629674b
Jatin Arora <jaarora45@gmaill.com>
2020-03-02 20:25:09 -0500

    Sample Code for fetching JSON and posting to DB

--- src/routing/recipe.js
+++ src/routing/recipe.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 var recipeController = require('../controllers/recipeController');
 
 // Routes for recipes
 router.get('/', function (req, res) {
-  res.send({ "router": "recipe" });
+  res.send({ "router": "recipe" }); 4
 });
 router.get('/detail/:id', recipeController.recipeDetail); // request a recipe detail by ID
 router.get('/search?*', recipeController.getRecipes); // search for recipes
 router.get('/create', recipeController.recipeCreatePOST); // POST Create Recipe
+router.post('/createRecipe', recipeController.recipeCreatePOST);
+/*
+router.post('/createRecipe', (req, res) => {
+  recipeController.recipeCreatePOST(req.body);
+
+});
+*/
 module.exports = router;
